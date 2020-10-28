588281252ad6cef2cd395622d5cdf52eecbb2b65
Kumar Vaibhav <kumar.vaibhav002@gmail.com>
2020-10-27 23:30:34 -0700

    Adding Placeholder Chart

--- memory-processor-simulator/src/app/app.module.ts
+++ memory-processor-simulator/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChartComponent } from './chart/chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartComponent
   ],
   imports: [
     BrowserModule,
