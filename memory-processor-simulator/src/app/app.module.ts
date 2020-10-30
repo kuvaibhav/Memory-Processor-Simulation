ee42c747f04896f98b7e82bad86e7d08dae1e191
Kumar Vaibhav <kumar.vaibhav002@gmail.com>
2020-10-29 23:23:38 -0700

    Adding Header and Footer

--- memory-processor-simulator/src/app/app.module.ts
+++ memory-processor-simulator/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ChartComponent } from './chart/chart.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChartComponent
+    ChartComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
