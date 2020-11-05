34b21d02f523b467c7054e469008c6668c7fd9f5
Kumar Vaibhav <kumar.vaibhav002@gmail.com>
2020-11-04 17:39:06 -0800

    Shade chnage

--- memory-processor-simulator/src/app/chart/chart.component.ts
+++ memory-processor-simulator/src/app/chart/chart.component.ts
@@ -21,7 +21,7 @@ export class ChartComponent implements OnInit {
               data: datum,
               backgroundColor: [
                   'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(55, 162, 235, 0.2)',
                   'rgba(255, 206, 86, 0.2)',
                   'rgba(75, 192, 192, 0.2)',
                   'rgba(153, 102, 255, 0.2)',
